7eab0c23e237df707f02cfff3606eee8867e8665
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-06-24 07:20:37 -0700

    fix tempfile bug on windows

--- JSAnimation/IPython_display.py
+++ JSAnimation/IPython_display.py
@@ -12,11 +12,20 @@ def anim_to_html(anim, fps=None, embed_frames=True, default_mode='loop'):
     if hasattr(anim, "_html_representation"):
         return anim._html_representation
     else:
-        with tempfile.NamedTemporaryFile(suffix='.html') as f:
-            anim.save(f.name, writer=HTMLWriter(fps=fps,
-                                                embed_frames=embed_frames,
-                                                default_mode=default_mode))
-            html = open(f.name).read()
+        # tempfile can't be used here: we need a filename, and this
+        # fails on windows
+
+        #with tempfile.NamedTemporaryFile(suffix='.html') as f:
+        #    anim.save(f.name, writer=HTMLWriter(fps=fps,
+        #                                        embed_frames=embed_frames,
+        #                                        default_mode=default_mode))
+        #    html = open(f.name).read()
+
+        filename = './tmp_output_fdsahkrkdkskrt.html'  # poor-man's temp file
+        anim.save(filename,  writer=HTMLWriter(fps=fps,
+                                               embed_frames=embed_frames,
+                                               default_mode=default_mode))
+        html = open(filename).read()
         anim._html_representation = html
         return html
 
